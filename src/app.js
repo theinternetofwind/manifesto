6858d9601f3fdb562800db4a4b372f6c139c6d79
Francesco Paraggio <francesco@windstack.io>
2016-08-26 16:19:31 +0200

    last five signatories populated from json

---
+++ src/app.js
@@ -0,0 +1,14 @@
+function creatediv(item){
+  return html =  '<div class="signature"><p class="name">'+ item.name + '</p><p class="organization">' + item.org + '</p><p class = "aim">'+ item.why +'</p></div>';
+}
+
+// function populating the signatories container with the data from json. slice(0,5) allow to show the first five.
+
+$( document ).ready(function() {
+  $.getJSON( "/signatories.json", function( data ) {
+    var items = data.items.slice(0,5).map(function (item) {
+          var div = creatediv(item);
+          $(div).appendTo("#signatures-container");
+        });
+      });
+});
